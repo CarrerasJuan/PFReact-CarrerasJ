a73159f7c642813605104866c5473d907b8d8ba9
CarrerasJE <148992169+CarrerasJuan@users.noreply.github.com>
2025-10-02 16:31:24 -0300

    Preparando deploy a GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,25 @@ import ItemListContainer from './components/ItemListContainer';
 import Navbar from './components/Navbar';
 import { CartProvider } from './context/CartContext';
 import './main.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 
 function App() {
-
   return (
-    <div>
     <CartProvider>
-      <BrowserRouter>
-         <Navbar/>
-
-           <Routes>
-             <Route path='/' element={<ItemListContainer/>} />
-             <Route path='/item/:id' element={<ItemDetailContainer/>} />
-             <Route path='/productos/:categoria' element={<ItemListContainer/>} />
-             <Route path='/contacto' element={<Contacto/>} />
-             <Route path='/carrito' element={<Carrito/>} />
-             <Route path="/checkout" element={<Checkout />}/>
-             {/* <Route path='/accesorios' element={<Accesorios />} /> */}
-             </Routes>
-             <Footer />  
-      </BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<ItemListContainer/>} />
+        <Route path='/item/:id' element={<ItemDetailContainer/>} />
+        <Route path='/productos/:categoria' element={<ItemListContainer/>} />
+        <Route path='/contacto' element={<Contacto/>} />
+        <Route path='/carrito' element={<Carrito/>} />
+        <Route path="/checkout" element={<Checkout />}/>
+        {/* <Route path='/accesorios' element={<Accesorios />} /> */}
+      </Routes>
+      <Footer />  
     </CartProvider> 
-    </div>
   )
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './main.css'
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //
-    <App />
-  //</React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 )
